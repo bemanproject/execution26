4fbef5c3bc891052493ba2d18e05befbf2b3913f
Maikel Nadolski <maikel.nadolski@gmail.com>
2024-11-21 08:06:01 +0100

    exec.split: improve tests

--- tests/beman/execution26/exec-split.test.cpp
+++ tests/beman/execution26/exec-split.test.cpp
@@ -34,7 +34,26 @@ void test_destroy_two_unused_split() {
     auto copy  = split;
 }
 
-void test_sync_wait_on_split() {
+using beman::execution26::detail::type_list;
+using beman::execution26::detail::meta::combine;
+
+template <class... Args>
+using to_set_vaule_t = type_list<beman::execution26::set_value_t(Args...)>;
+
+void test_completion_sigs_and_sync_wait_on_split() {
+    auto just          = beman::execution26::just(NonCopyable{});
+    auto split         = beman::execution26::split(std::move(just));
+    using split_sender = std::decay_t<decltype(split)>;
+    struct empty_env {};
+    using expected_value_completions = type_list<beman::execution26::set_value_t(const NonCopyable&)>;
+    using value_completions = beman::execution26::value_types_of_t<split_sender, empty_env, to_set_vaule_t, combine>;
+    static_assert(std::same_as<value_completions, expected_value_completions>);
+
+    auto eat_completion = beman::execution26::then(split, [&](const NonCopyable&) {});
+    ASSERT(beman::execution26::sync_wait(eat_completion));
+}
+
+void test_two_sync_waits_on_one_split() {
     auto        just    = beman::execution26::just(NonCopyable{});
     auto        split   = beman::execution26::split(std::move(just));
     const void* pointer = nullptr;
@@ -47,13 +66,8 @@ void test_sync_wait_on_split() {
 }
 
 TEST(exec_split) {
-    {
-        // Test that split is a sender_in an empty_env
-        // This also tests
-        auto just               = beman::execution26::just(NonCopyable{});
-        auto copyable           = beman::execution26::split(std::move(just));
-        using split_sender_type = decltype(copyable);
-        struct empty_env {};
-        static_assert(beman::execution26::sender_in<split_sender_type, empty_env>);
-    }
+    test_destroy_unused_split();
+    test_destroy_two_unused_split();
+    test_completion_sigs_and_sync_wait_on_split();
+    test_two_sync_waits_on_one_split();
 }
